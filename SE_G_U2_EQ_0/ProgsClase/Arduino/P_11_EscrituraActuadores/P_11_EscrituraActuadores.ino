57414adb424f58137172901877d914c0bf6cedcc
ahgarciar <ahgarcia@docentes.uat.edu.mx>
2023-02-21 08:55:53 -0600

    Actualizado

---
+++ SE_G_U2_EQ_0/ProgsClase/Arduino/P_11_EscrituraActuadores/P_11_EscrituraActuadores.ino
@@ -0,0 +1,56 @@
+//Actuadores analogicos. -> PWM
+
+int actuador1 = 3;
+int actuador2 = 5;
+int actuador3 = 6;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.setTimeout(10);  
+}
+
+int vA, vB, vC;
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>0){
+    String c = Serial.readString();
+    //Serial.println(c); //confirmacion de la lectura recibida
+    //E001R056R300C    
+    if(c.length() == 13){ //cadena completa      
+      if(c.charAt(0)=='E' && c.charAt(c.length()-1)=='C'){ //segunda validacion   
+          //E001R056R300C       
+          c = c.substring(1,c.length()-1) + 'R';
+          Serial.println("L:"+c + "T");
+          int cont = 0;
+          String temp="";
+          for(int i = 0; i<c.length();i++){
+            if(c.charAt(i)!='R'){
+              temp += c.charAt(i);
+            }
+            else{
+              switch(cont){
+                case 0:
+                    vA = temp.toInt();
+                break;
+                case 1:
+                    vB = temp.toInt();
+                break;
+                case 2:
+                    vC = temp.toInt();
+                break;
+              }
+              temp = "";
+              cont++;
+            }
+          }
+
+          Serial.println(String(vA) + "   " + String(vB) + "   "+ String(vC));
+
+      }
+    }
+
+  }
+
+  delay(100);
+}
